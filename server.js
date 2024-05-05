9d0e0ae784611697af4655c6b98d222122b829fa
ShaniyaG <110322056+ShaniyaG@users.noreply.github.com>
2024-05-05 19:04:33 +0000

    Registeration changes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
+const User = require('./model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 const app = express();
 app.use(express.json());
@@ -10,6 +14,37 @@ const PORT = process.env.PORT || 8001;
 
 app.get('/',async (req,res)=>{
     try{
+        const { firstname,lastname,email,password } = req.body;
+
+        if(!(firstname && lastname && email && password)){
+            res.status(400).json({message:'All fields are mandatory'})
+        }
+        const existingUser =  await User.findOne({email});
+        
+        if(existingUser){
+            res.status(401).json({message:'User already exist'}) 
+        }
+        const  hashedPassword = await bcrypt(password,8);
+       const user = await User.create({
+            firstname,
+            lastname,
+            email,
+            password:hashedPassword,
+
+        })
+        const token = jwt.sign(
+            {
+                id:user._id
+            },
+            'shhhh',
+            {
+                expiresIn:"2h"
+            }
+        )
+        user.token = token;
+        user.password = undefined;
+
+        res.status(200).json(user);
 
     }catch(error){
         console.log(error);
